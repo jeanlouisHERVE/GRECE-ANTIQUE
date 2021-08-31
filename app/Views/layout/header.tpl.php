2c4d1fca4ed1a70cbbeeca7a231bab739de204b8
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-08-31 21:01:07 +0200

    mise en place de modules js

--- app/Views/layout/header.tpl.php
+++ app/Views/layout/header.tpl.php
@@ -34,6 +34,12 @@
     <!-- css -->
     <link rel="stylesheet" type="text/css" href="/GRECE-ANTIQUE/public/assets/css/normalize.css" />
     <link rel="stylesheet" type="text/css" href="/GRECE-ANTIQUE/public/assets/css/style.css" />
+
+
+    <!-- javascript -->
+    <script defer type="text/javascript" src="/GRECE-ANTIQUE/public/assets/js/app.js"></script>
+    <script defer type="text/javascript" src="/GRECE-ANTIQUE/public/assets/js/navbar.js"></script>
+    
 </head>
 
 <!-- ************************navbar************************ -->
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,38 +1,13 @@
 
-const module = {
+const app = {
 
   init: function() {
 
-    const title = document.querySelector('.nav-link--article');
-   
-    title.addEventListener('click', module.handleFormSubmit);
-    
+    console.log("App Init !");
+    navbar.init();
+        
   },
 
-  handleFormSubmit: function(evt) {
-    
-   
-    evt.preventDefault();
-
-    const parent = document.querySelector('.navbar--container');
-
-    
-
-    const contenu = document.querySelector('.contenu');
-
-    if (contenu.classList.contains('d-none')) {
-     
-      contenu.classList.replace ('d-none','d-flex');
-      parent.classList.add('open');
-
-    } else if (contenu.classList.contains('d-flex')) {
-      
-      parent.classList.remove('open');
-      contenu.classList.replace ('d-flex','d-none');
-
-    }
-
-  }
 }
 
-document.addEventListener('DOMContentLoaded', module.init);
+document.addEventListener('DOMContentLoaded', app.init);
