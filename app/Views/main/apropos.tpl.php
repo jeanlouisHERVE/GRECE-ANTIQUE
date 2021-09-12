51f089bede85b285a848ee5be25b4503eda11cbf
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-09-12 22:29:50 +0200

    mise en page de la template apropos

--- app/Views/main/apropos.tpl.php
+++ app/Views/main/apropos.tpl.php
@@ -10,16 +10,20 @@
   <!-- ************************container************************ -->
 
   <div class="container main--container--apropos col-12 p-0 d-flex">
-    <aside class="left--apropos col-6 p-3">
-        <h2 class="apropos--websitetitle">
-          grece antique
-        </h2>
-        <h1 class="apropos--title">
-          A propos
-        </h1>
+    <aside class="left--apropos col-6 p-5">
+      <div class="text--apropos">
+        <h1 class="apropos--title">A propos</h1>
+        <div class="apropos--content d-flex flex-row">
+          <img class="apropos--achille" src="<?= $absoluteUrl ?>assets/img/achille.jpg" alt="">
+          <div class="apropos--paragraphe">
+            <p>Le site "Grece Antique" est né en 2009 alors que étudiant en classe préparatoire aux grandes écoles, j'ai ressenti le besoin de transmettre ma passion au plus grand nombre. Cette première version est restée en ligne jusqu'en 2012</p>
+            <p>En 2021, j'ai retrouvé les articles publiés dix ans plus tôt sur ce site et j'ai décidé de rebâtir et de moderniser "Grece Antique". J'espère que vous l'apprécierez </p>
+          </div>
+        </div>
+      </div>
     </aside>
-    <aside class="right--apropos col-6">
-        <div class="apropos--picture"></div>
+    <aside class="right--apropos p-0 col-6">
+      <div class="apropos--picture "></div>
     </aside>
-    
+
   </div>
