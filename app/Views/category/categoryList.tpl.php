0b9fe855f9e588e40411e8c806ebc155d2b4fd07
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-09-18 13:54:48 +0200

    categoryList responsive v3

--- app/Views/category/categoryList.tpl.php
+++ app/Views/category/categoryList.tpl.php
@@ -9,30 +9,30 @@
 
     <!-- ************************container************************ -->
 
-    <div class="container main--container col-12">
+    <div class="container category--list--main--container col-12 p-0">
 
-            <div class="right  categorie--list--right col-12 d-flex flex-row align-items-between">
-                <div class="divTitle col-4">
-                    <h1 class="categorie--list--main--title ">
+            <div class="right  category--list--right col-12 d-flex flex-row align-items-between">
+                <div class="divTitle col-12 col-md-11 col-lg-4 ">
+                    <h1 class="category--list--main--title ">
                         Liste des categories
                     </h1>
-                </div>
-                <div class="categorie--list--container col-7 d-flex flex-column justify-content-center align-items-center ">
-                    <?php foreach ($categories as $category) : ?>
-                        <?php $categories ?>
-                        <a href="<?= $router->generate('articlesByCategory', ['id' => $category->getId()]) ?>" class=" btn d-flex col-6">
-                            <div class="category--list--unity d-flex flex-row justify-content-between">
-                                <div class="category--list--h2">
-                                    <h2 class="category--list--name"><?= $category->getName() ?></h2>
+                    <div class="category--list-container d-flex flex-column">
+                        <?php foreach ($categories as $category) : ?>
+                            <?php $categories ?>
+                            <a href="<?= $router->generate('articlesByCategory', ['id' => $category->getId()]) ?>" class=" btn d-flex col-10">
+                                <div class="category--list--unity d-flex flex-row justify-content-between align-items-center">
+                                    <div class="category--list--h2">
+                                        <h2 class="category--list--name"><?= $category->getName() ?></h2>
+                                    </div>
+                                    <div class="category--list--arrow d-md-none d-flex justify-content-center align-items-center">
+                                        <i class="fas fa-chevron-right category--list--arrow--i"></i>
+                                    </div>
                                 </div>
-                                <div class="category--list--arrow d-flex justify-content-center align-items-center">
-                                    <i class="fas fa-chevron-right category--list--arrow--i"></i>
-                                </div>
-                            </div>
-                        </a>
-                    <?php endforeach; ?>
+                            </a>
+                        <?php endforeach; ?>
+                    </div>
                 </div>
-                <div class="vide col-1"></div>
+                <div class="vide col-md-1 col-lg-8"></div>
             </div>
 
 
