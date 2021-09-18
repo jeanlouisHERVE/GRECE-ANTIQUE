5a0cde029c34ebd3b0e1cc114c8e80861697386f
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-09-18 10:30:42 +0200

    responsive articleList

--- app/Views/article/articleList.tpl.php
+++ app/Views/article/articleList.tpl.php
@@ -9,14 +9,14 @@
 
     <!-- ************************container************************ -->
 
-    <div class="list--article--main--container col-12 ">
-        <div class="list--article--under--navbar--picture col-12"></div>
-        <div class="list--article--hfilter col-12 flex-sm-column justify-content-center align-items-center flex-md-row ">
-            <div class="list--article--h1 col-sm-12 col-md-4">
-                <h1 class=" col-sm-12 text-center">Liste des articles</h1>
+    <div class="list--article--main--container col-12 d-flex flex-column justify-content-center">
+        <div class="list--article--under--navbar--picture "></div>
+        <div class="list--article--hfilter d-flex justify-content-center align-items-center justify-content-md-between">
+            <div class="list--article--h1 col-sm-6 col-md-4">
+                <h1 class="text-left">Liste des articles</h1>
             </div>
-            <div class="list--article--top--menu--right flex-sm-row col-12 justify-content-center align-self-right col-md-4">
-                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+            <div class="list--article--top--menu--right col-md-4">
+                <div class="btn-group btn-group-toggle " data-toggle="buttons">
                     <label class="filters filters--all btn btn-outline-primary active">
                         <input type="radio" name="options" id="option1" autocomplete="off" checked> A - Z
                     </label>
@@ -29,13 +29,13 @@
                 </div>
             </div>
         </div>
-        <div class="list--article--under--picture d-flex justify-content-center col-sm-12 p-0 mt-3">
+        <div class="list--article--under--picture d-flex justify-content-center col-sm-12 p-0 mt-3 col-md-10">
             <table class="table d-flex flex-column justify-content-center col-12 p-0">
                 <tbody class="col-12 p-0">
                     <?php foreach ($titleList as $title) : ?>
                         <tr class="list--article--unity d-flex justify-content-center col-12 p-0 flex-row">
-                            <td class="list--article--column--category col-1"> <span class="badge categoryBadge "><?= $title->getCategory()->getName() ?></span></td>
-                            <td class="list--article--column--title col-8">
+                            <td class="list--article--column--category col-1 d-sm-none d-md-block"> <span class="badge categoryBadge "><?= $title->getCategory()->getName() ?></span></td>
+                            <td class="list--article--column--title col-11 col-md-8">
                                 <a class="list--article--a" href="<?= $router->generate('articleById', ['id' => $title->getNumber()]) ?>">
                                     <h2><?= $title->getTitle() ?></h2>
                                 </a>
