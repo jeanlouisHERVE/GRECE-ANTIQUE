c300adbc7cdef281f8e6b51dac04f7d12e1991f4
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-09-21 21:43:08 +0200

    navbar principale test avec module de recherche

--- app/Views/partials/nav.tpl.php
+++ app/Views/partials/nav.tpl.php
@@ -1,30 +1,32 @@
-<nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light justify-content-between">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light pr-3 pl-3">
+  <a class="navbar-brand" href="#">Grece Antique</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-    <div class="collapse navbar-collapse bg-primary" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="<?= $router->generate('main-home') ?>">Accueil <span class="sr-only">(current)</span></a>
-            </li>
-            
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Articles
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="<?= $router->generate('article-list') ?>">Liste complète</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= $router->generate('article-categories') ?>">Catégories</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= $router->generate('aPropos') ?>">A propos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= $router->generate('contact') ?>">Contact</a>
-            </li>
-        </ul>
-    </div>
+  <div class="collapse collapsing navbar-collapse bg-light " id="navbarSupportedContent" >
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Articles</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Catégories</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">A propos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-primary my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+    </form>
+  </div>
 </nav>
 
 
