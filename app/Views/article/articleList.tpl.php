81d0f6ca9a9be8924b33176fc4b681ca2fcaeef0
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2022-01-05 21:01:03 +0100

    modify responsive medium size of the articleList

--- app/Views/article/articleList.tpl.php
+++ app/Views/article/articleList.tpl.php
@@ -12,20 +12,12 @@
     <div class="list--article--main--container col-12 d-flex flex-column justify-content-center">
         <div class="list--article--under--navbar--picture "></div>
         <div class="list--article--header d-flex justify-content-center align-items-center justify-content-md-between">
-            <div class="list--article--h1 col-sm-6 col-md-4">
+            <div class="list--article--h1 col-sm-7 col-md-5 col-lg-8">
                 <h1 class="text-left">Liste des articles</h1>
             </div>
-            <div class="list--article--top--menu--right col-md-4">
-                <div class="btn-group btn-group-toggle " data-toggle="buttons">
-                    <!-- <label class="filters filters--all btn btn-outline-primary active">
-                        <input type="radio" name="options" id="option1" autocomplete="off" checked> A - Z
-                    </label>
-                    <label class="filters filters--date btn btn-outline-primary">
-                        <input type="radio" name="options" id="option3" autocomplete="off"> Date
-                    </label> -->
-                    <div class="filters filters--category">
-                        <a class="filters__choice" href="<?= $router->generate('article-categories') ?>">Voir les catégories</a>
-                    </div>
+            <div class="list--article--top--menu--right col-md-5">
+                <div class="filters filters--category">
+                    <a class="filters__choice" href="<?= $router->generate('article-categories') ?>">Voir les catégories</a>
                 </div>
             </div>
         </div>
@@ -34,13 +26,13 @@
                 <tbody class="col-12 p-0">
                     <?php foreach ($titleList as $title) : ?>
                         <tr class="list--article--unity d-flex justify-content-center col-12 p-0  flex-row">
-                            <td class="list--article--column--category d-none d-md-flex justify-content-start col-1"> <span class="badge categoryBadge"><?= $title->getCategory()->getName() ?></span></td>
+                            <td class="list--article--column--category d-none d-lg-flex justify-content-start col-1"> <span class="badge categoryBadge"><?= $title->getCategory()->getName() ?></span></td>
                             <td class="list--article--column--title d-flex justify-content-start align-items-center col-11 col-md-8 ">
                                 <a class="list--article--a" href="<?= $router->generate('articleById', ['id' => $title->getNumber()]) ?>">
                                     <h2 class="list--article--column--title--main"><?= $title->getTitle() ?></h2>
                                 </a>
                             </td>
-                            <td class="list--article--column--date d-none d-md-flex col-md-1 justify-content-end ">
+                            <td class="list--article--column--date d-none d-md-flex col-md-2 justify-content-end ">
                                 <p><?= $title->getCreatedAt("d-m-Y") ?></p>
                             </td>
                         </tr>
